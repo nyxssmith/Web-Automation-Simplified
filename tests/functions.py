c9ec11d0488e1881e819b94cb5687f726fd125fe
Nick Smith <nick.smith@klaviyo.com>
2020-05-01 16:48:24 -0400

    updates 	modified:   test-data/test-template.json 	modified:   tests/Test_Microcenter_home_page.py 	modified:   tests/functions.py

--- tests/functions.py
+++ tests/functions.py
@@ -48,7 +48,119 @@ class actions:
     """
 
     def get_xpath_as_string_for_body_element(self, element_string, driver):
-        verbose = False
+        verbose = True
+
+        # get body onwards with no breaks
+        src = driver.page_source[driver.page_source.find("</head>") + 7:].replace("\t", "").replace("><",
+                                                                                                    ">\n<").replace(
+            "> <", ">\n<")
+        #print(src)
+
+        src = src.encode('ascii', 'ignore').decode('ascii')
+        print(str(src).find(element_string))
+        element_string_location = str(src).find(element_string)
+        element_string_location_end = str(src)[element_string_location:].find('>') + element_string_location+1
+        print(element_string_location)
+        print(element_string_location_end)
+        print(str(src)[:element_string_location_end])
+        #print(str(src).find(element_string))
+        #print(str(src)[:str(src).find(element_string)])
+        #print(str(src)[:str(src).find(element_string)])
+        exit(0)
+        # get all index of the occurance of string, in case its search or somehting commong
+
+        # make the element str regex safe
+        regex_safe_element_string = element_string.replace("(", "\(").replace(")", "\)").replace(".", "\.").replace("+",
+                                                                                                                    "\+").replace(
+            ">", "\>").replace("<", "\<").replace("-", "\-")
+        # get all indexes when the string shows up, like "search" and "search for"
+        list_of_index_of_element_strings = [m.start() for m in re.finditer(regex_safe_element_string, src)]
+        # list_of_index_of_element_strings = [m.start() for m in re.finditer('(?=%s)(?!.{1,%d}%s)' % (regex_safe_element_string, len(regex_safe_element_string)-1, regex_safe_element_string), src)]
+        # print(list_of_index_of_element_strings)
+
+        # if the list is epmpy, elementi isnt found, so retrun placeholder
+        if list_of_index_of_element_strings == []:
+            return "html/err"
+        # for each possible index
+        # print("list of possible indices",list_of_index_of_element_strings)
+        for index in list_of_index_of_element_strings:
+
+            # check that right after the target string, is a <
+            # if its a space, it could be "search for" rather than "search" which is wrong
+            descion_string = src[index:index + len(element_string) + 20]
+            # print("d str 1",descion_string)
+
+            descion_string = descion_string.replace("\n", "").replace(" <", "<")
+            # if its decied to be correct, retrun corect one
+            # print("d str", descion_string)
+            # print("index ", index)
+            if descion_string[descion_string.find(element_string) + len(element_string):][0] == "<":
+                index_of_element_string = index
+                break
+        # get the html, and cut it off at the string we want
+        up_to_element_string = src[:index_of_element_string]
+        # split on new lines
+        split = up_to_element_string.split("\n")
+        split.remove('')
+
+        parent_tree = []
+        for line in split:
+            if line != '':
+                if verbose: print("-=====================")
+                if verbose: print("starting p tree", parent_tree)
+
+                # src_dict[counter]["line"] = line
+                regex_result_open = re.findall('\<(.*?)\ ', line)
+
+                regex_result_open_ends = re.findall('\<(.*?)>', line)
+                # print("rw oepn ends",regex_result_open_ends)
+
+                for result in regex_result_open_ends:
+                    # print(result)
+                    # print("/" in result)
+                    # print(" " in result)
+                    if (not ("/" in result)) and (not (" " in result)):
+                        # print("keep ",result)
+                        regex_result_open.append(result)
+
+                # print("r oepn ends",regex_result_open_ends)
+
+                # add all opened tags to the parent tree
+                # dont add input, br or invalid into the parent list, they have been dealt with elsewhere
+                for result in regex_result_open:
+                    if result != 'input' and result != 'br' and result != 'hr' and (
+                            not ((">" or "<") in result) and "--" not in result):
+                        parent_tree.append(result)
+
+                regex_result_close = re.findall('</(.*?)\>', line)
+                # check if a link was added to the close section, adn rm it, as its a mistake
+                for result in regex_result_close:
+                    if (line[line.find(result) - 7:line.find(result)] == "href=\"/"):
+                        regex_result_close.remove(result)
+                    if result == '"' or result == '/"':
+                        regex_result_close.remove(result)
+                # remove any closed parts from the parent tree
+                for result in regex_result_close:
+                    if result == parent_tree[-1]:
+                        parent_tree.pop()
+
+                if verbose: print("line", line)
+
+                if verbose: print("open", regex_result_open)
+                if verbose: print("close", regex_result_close)
+                if verbose: print("end p tree", parent_tree)
+
+                if verbose: print("------------------------\n\n")
+        if verbose: print("final p treee", parent_tree)
+
+        base_list = ["html"]
+        xpath_list = base_list + parent_tree
+        if verbose: print("xpath list", xpath_list)
+
+        return "/".join(xpath_list)
+
+    def get_xpath_as_string_for_body_element_old(self, element_string, driver):
+        verbose = True
 
         # get body onwards with no breaks
         src = driver.page_source[driver.page_source.find("</head>") + 7:].replace("\t", "").replace("><",
@@ -146,6 +258,7 @@ class actions:
         if verbose: print("xpath list", xpath_list)
 
         return "/".join(xpath_list)
+
 
     # make webdriver object
     def create_driver(self, chrome=False):
